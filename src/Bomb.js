f315b5629f421dcec1324414e12ccfafbf11d0a4
austin_smith <trunk9slamchest@gmail.com>
2019-11-19 12:21:59 -0500

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,30 @@
-// your Bomb code here!
+import React from 'react';
+
+
+export default class Bomb extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.state = {
+			secondsLeft: this.props.initialCount
+		}
+	}
+
+	decrement_count = () => {
+		const decrement_count = this.state.secondsLeft - 1
+		this.state({
+			secondsLeft: decrement_count
+		})
+	}
+
+	render(){
+
+		const countdown = this.state.secondsLeft !== 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : 'Boom!'
+
+		return(
+				<div>
+					{ countdown }
+				</div>
+		)
+	}
+}
